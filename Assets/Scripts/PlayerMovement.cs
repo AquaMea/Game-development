a07602b9050b59e2e857d0b35e0972f259c70911
digmo <lee-ferwerda@hotmail.com>
2018-12-19 00:48:05 +0100

    Added collision and a timed powerup

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    [SerializeField]
-    private float Speed;
-
-    //private Vector2 direction;
-
+    public float Speed;
     public Animator animator;
 
 	// Update is called once per frame
 	void Update ()
     {
-        //Move();
-        //GetInput();
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"), 0.0f);
 
         animator.SetFloat("Horizontal", movement.x);
@@ -26,31 +20,3 @@ public class PlayerMovement : MonoBehaviour {
         transform.position = transform.position + movement * Time.deltaTime * Speed;
     }
 }
-
-    /*public void Move()
-    {
-        transform.Translate(direction*Speed*Time.deltaTime);
-    }
-
-private void GetInput()
-{
-    direction = Vector2.zero;
-
-    if (Input.GetKey(KeyCode.W))
-    {
-        direction += Vector2.up;
-    }
-    if (Input.GetKey(KeyCode.A))
-    {
-        direction += Vector2.left;
-    }
-    if (Input.GetKey(KeyCode.S))
-    {
-        direction += Vector2.down;
-    }
-    if (Input.GetKey(KeyCode.D))
-    {
-        direction += Vector2.right;
-    }
-}
-}*/
---
+++ Assets/Scripts/SpeedUp.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedUp : MonoBehaviour
+{
+
+    public float multiplier = 2;
+    public float duration = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            StartCoroutine(Pickup(other));
+        }
+    }
+
+    IEnumerator Pickup(Collider2D player)
+    {
+        PlayerMovement playermovement = player.GetComponent<PlayerMovement>();
+        playermovement.Speed *= multiplier;
+
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<BoxCollider2D>().enabled = false;
+
+        yield return new WaitForSeconds(duration);
+
+        playermovement.Speed /= multiplier;
+
+        Destroy(gameObject);
+    }
+}
