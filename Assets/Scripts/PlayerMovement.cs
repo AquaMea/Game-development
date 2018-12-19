e9f24bd16355383e8f258a13055d910c228584ca
digmo <lee-ferwerda@hotmail.com>
2018-12-19 19:21:20 +0100

    Fixed pointers of sprites + smooth collsion and depth

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,7 @@ public class PlayerMovement : MonoBehaviour {
     public Animator animator;
 
 	// Update is called once per frame
-	void Update ()
+	void FixedUpdate ()
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"), 0.0f);
 
